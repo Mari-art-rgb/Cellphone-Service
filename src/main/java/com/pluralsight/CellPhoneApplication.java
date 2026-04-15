0baaae4160c537136c5e2eaa86904f252ee74ae6
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-15 11:12:02 -0400

    Initial Commit

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your phone's serial number? ");
+        long serialNumber = scanner.next();
+
+
+        serialNumber = 0;
+        model = "";
+        carrier = "";
+        phoneNumber = "";
+        owner = "";
+
+        theirPhone
+
+    }
 }
