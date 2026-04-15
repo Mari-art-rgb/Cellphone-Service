778c5a8645f08e6d2f77e13d8f5dcf83d995d957
Mari-art-rgb <meggisonariela@gmail.com>
2026-04-15 11:29:10 -0400

    Initial Commit Completed but it is not letting user put in their carrier, it just skips. Fixed, code is complete.

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -11,18 +11,23 @@ public class CellPhoneApplication {
 
         System.out.println("What is your phone's serial number? ");
         int serialNumber = scanner.nextInt();
+        scanner.nextLine();
 
         System.out.println("What is your phone model: ");
         String model = scanner.next();
+        scanner.nextLine();
 
         System.out.println("Who is your phone carrier: ");
         String carrier = scanner.next();
+        scanner.nextLine();
 
         System.out.println("What is your phone number: ");
         String phoneNumber = scanner.next();
+        scanner.nextLine();
 
         System.out.println();
         String owner = scanner.next();
+        scanner.nextLine();
 
         serialNumber = 0;
         model = "";
